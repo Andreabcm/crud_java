881cfffac9702a657beec27c415a22727600daec
Andrea Cevallos <andreabcm16@gmail.com>
2023-04-27 00:13:40 +0200

    include data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT
+INTO
+  products
+  (title, text, img)
+VALUES
+  ('vhvb', 'uygtjyg', 'kjhkuh');
+INSERT
+INTO
+  products
+  (title, text, img)
+VALUES
+  ('dfsgdg', 'dfgfdg', 'dfgdfgfd');
